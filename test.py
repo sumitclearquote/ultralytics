80a70686065266745c0de6214343af02acec3484
Sumit <sumit@clearquote.io>
2024-03-10 11:38:47 +0530

    Added testing for v2

--- test.py
+++ test.py
@@ -8,8 +8,8 @@ import torch
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 
-model_type = "v1_s"
-data = "spinny" #[val, 'spinny]
+model_type = "v2_s"
+data = "val" #[val, 'spinny]
 
 
 
@@ -18,7 +18,7 @@ model = YOLO(model_path)
 
 conf = float(sys.argv[2])
 iou_nms_thresh = float(sys.argv[1]) #default=0.7
-batch_size = 160 if model_type == "v1_s" else 192 if model_type=="v1_n" else 1
+batch_size = 160 if model_type.endswith("s") else 192 if model_type.endswith("n") else 74 if model_type.endswith("m") else 1
 
 if data=='val': #Use validation set
         data_path =  "/home/paintfrmladmin01/datadrive/LPBlur/datasets/LP_yolo_dataset/lp_data.yaml"
@@ -36,7 +36,7 @@ config = {'data': data_path,
         'save_json' : True, 
         'save_txt' : True,
         'project' : f"my_runs/lpblur/{model_type}/val", 
-        'name' : f"spinny_val_analysis_{iou_nms_thresh}_{conf}"
+        'name' : f"val_analysis_{iou_nms_thresh}_{conf}"
 
         }
 
