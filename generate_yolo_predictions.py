1ff6a835d3861456f96190dfc06b33d872f4f0f0
Sumit <sumit@clearquote.io>
2024-03-16 15:25:41 +0530

    Testing v3_m with audit

--- generate_yolo_predictions.py
+++ generate_yolo_predictions.py
@@ -60,10 +60,10 @@ def generate_predictions(imgdir, model, dest_dir, conf_threshold = None, iou_nms
 
 
 if __name__ == '__main__':
-    iterations = ["v2_m"]
+    iterations = ["v3_m"]
     save_results = True
-    dtype = "quotes200" #[val, spinny2, "audit", "quotes200"]
-    conf_thresholds = [0.45]
+    dtype = "audit" #[val, spinny2, "audit", "quotes200"]
+    conf_thresholds = [0.1, 0.2, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6,0.65, 0.7, 0.8]
     iou_nms_thresh = 0.7
 
 
