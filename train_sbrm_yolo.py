453ace0525797103ee30143d9b7bc1d6c572faa9
Sumit <sumit@clearquote.io>
2024-04-30 23:21:07 +0530

    fsd

--- train_sbrm_yolo.py
+++ train_sbrm_yolo.py
@@ -31,7 +31,7 @@ def __init__(self, p=1.0):
         #Add custom augmentation here
         T += [  A.HorizontalFlip(p=0.7),
                 A.VerticalFlip(p=0.2),
-                #A.RandomSizedBBoxSafeCrop(height= 450, width=800, erosion_rate=0.3, p = 0.3),
+                A.RandomSizedBBoxSafeCrop(height= 450, width=800, erosion_rate=0.3, p = 0.3),
                 #A.Affine(scale=(0.9, 1.8), shear=(-20, 20), rotate=(-180,180),  p = 0.2),
                 #A.Perspective(p = 0.1),
                 #A.ChannelShuffle(p = 0.15),
