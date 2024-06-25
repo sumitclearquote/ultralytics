78b11a1a9a08313b3c372def95378e8cb370c923
Sumit <sumit@clearquote.io>
2024-06-25 17:38:21 +0530

    awerg

--- train_headcam_yolo.py
+++ train_headcam_yolo.py
@@ -130,7 +130,7 @@ for train_version in train_versions:
 
 
     config  ={  'data': f"/home/paintfrmladmin01/datadrive/ssqs/datasets/{dataset_name}/{yolo_cfg}", 
-                'epochs': 120,
+                'epochs': 1,
                 'lr0':lr, #default is 1e-3
                 'batch': bsize,
                 'imgsz':imgsize,
