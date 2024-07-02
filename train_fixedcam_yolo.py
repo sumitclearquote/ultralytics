c590b71e91c11010467a72dbea60bd83bb5ef127
Sumit <sumit@clearquote.io>
2024-07-02 21:00:44 +0530

    wrb

--- train_fixedcam_yolo.py
+++ train_fixedcam_yolo.py
@@ -87,7 +87,7 @@ for imgsize in imgsizes:
             model_file = "yolov8s.yaml"
             lr = 0.001
             if imgsize == 640:
-                bsize = 88 #128fullheadcam
+                bsize = 84 #128fullheadcam
             elif imgsize == 480:
                 bsize = 80 #80
                 
