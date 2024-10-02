cdf4c618393b5c80ce4ea0bffdb657a6b1a51076
Sumit <sumit@clearquote.io>
2024-10-02 22:44:50 +0530

    argbr

--- train_lpscan.py
+++ train_lpscan.py
@@ -120,7 +120,7 @@ dataset_name = "final_ocr_dataset_v1" #wheelrim and lifting pads were expanded b
 yolo_cfg = "lp_ocr_data.yaml" #name of the yolo cfg yaml file inside dataset
 
 # HYPERPARAMETERS
-epochs = 350
+epochs = 1 #350
 patience = 100 # After how many epochs to stop training if results do not improve,.
 train_versions =  ["v1_n", "v1_p_n"]         #["v1_n", "v1_p_n", "v1_s", "v1_p_s"]  #-original
 imgsizes = [224, 320, 400]    #-original
